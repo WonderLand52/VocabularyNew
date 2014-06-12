e4a5724135158ce0411de88b24b08529516c6cd1
WonderLand52 <illiaRudenko52@gmail.com>
2014-06-12 18:07:28 +0300

    прога бросает НуллПоинтер (метод getCount())

--- app/src/main/java/com/rudenkoInc/vocabularynew/app/ExerciseActivity.java
+++ app/src/main/java/com/rudenkoInc/vocabularynew/app/ExerciseActivity.java
@@ -1,6 +1,7 @@
 package com.rudenkoInc.vocabularynew.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class ExerciseActivity extends Activity implements View.OnClickListener {
@@ -17,14 +21,17 @@ public class ExerciseActivity extends Activity implements View.OnClickListener {
     private final String LOG_TAG = "my logs";
 
     TextView tvNextWord;
-
     Button btnSubmit;
-
     EditText etTranslate;
 
+    DBHelper dbHelper;
+    SQLiteDatabase db;
+    Cursor c;
 
+    Random rand = new Random();
+    int count = 0;
 
-
+    static List<String> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +44,52 @@ public class ExerciseActivity extends Activity implements View.OnClickListener {
         btnSubmit = (Button)findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(this);
 
-        DBHelper dbHelper = new DBHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        Cursor c = db.query("myWords", null, null, null, null, null, null);
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getWritableDatabase();
+        c = db.query("myWords", null, null, null, null, null, null);
+        results = new LinkedList<String>();
 
-        Random rand = new Random();
         if(c != null){
             Log.d(LOG_TAG, String.valueOf(c.getCount()));
-
             if(c.moveToPosition(rand.nextInt(c.getCount()))){
-                String nextWord = c.getString(c.getColumnIndex("translation"));
-                tvNextWord.setText(nextWord);
+                setTvNextWord(tvNextWord, c);
             }
-            c.close();
         } else
             Log.d(LOG_TAG, "Cursor is null");
     }
 
     @Override
     public void onClick(View view) {
+            if(c != null){
+                if(c.moveToPosition(rand.nextInt(c.getCount()))){
+                    if(count < c.getCount()) {
+                        setTvNextWord(tvNextWord, c);
+                        results.add(etTranslate.getText().toString());
+                        count++;
+
+                    } else {
+                        Intent intent = new Intent(this, ResultsActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            } else
+                Toast.makeText(this, "Cursor is empty", Toast.LENGTH_SHORT).show();
+
 
     }
-}
+
+    private void setTvNextWord(TextView tv, Cursor c){
+        String nextWord = c.getString(c.getColumnIndex("translation"));
+        tv.setText(nextWord);
+    }
+
+    public List<String> getOriginals(){
+        return results;
+    }
+
+
+
+
+
+
+    }
---
+++ app/src/main/java/com/rudenkoInc/vocabularynew/app/ResultsActivity.java
@@ -0,0 +1,59 @@
+package com.rudenkoInc.vocabularynew.app;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class ResultsActivity extends Activity implements View.OnClickListener {
+
+    private final String LOG_TAG = "my logs";
+
+    TextView tvPercent;
+
+    int count = 1;
+    DBHelper dbHelper = new DBHelper(this);
+    SQLiteDatabase db;
+    Cursor c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+
+        tvPercent = (TextView)findViewById(R.id.tvPercent);
+
+        if(c != null){
+            if(c.moveToFirst()) {
+                do {
+                    db = dbHelper.getWritableDatabase();
+                    c = db.query("myWords", null, null, null, null, null, null);
+                    String original = c.getString(c.getColumnIndex("original"));
+                    for(String result: ExerciseActivity.results){
+                        if(original.equalsIgnoreCase(result))
+                            count++;
+                    }
+                }while (c.moveToNext());
+            }
+        }
+        Log.d(LOG_TAG, String.valueOf(c.getCount()));
+        c.close();
+
+//        tvPercent.setText(String.valueOf(countResult()));
+
+    }
+
+    void countResult(){
+        Log.d(LOG_TAG, String.valueOf(count));
+        Log.d(LOG_TAG, String.valueOf(c.getCount()));
+//        return count * 100/c.getCount();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
